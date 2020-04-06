7de0381f86a9e34b17e153a2a7eca60619f31c56
Amey Deotale <22147603+GenericCoder19@users.noreply.github.com>
2020-04-06 19:00:06 -0400

    Added files

--- covidmap/gaborone/models.py
+++ covidmap/gaborone/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from djgeojson.fields import PolygonField
 
 # Create your models here.
+class MapRegion(models.Model):
+    region_name = models.CharField(max_length=200)
+    current_cases = models.IntegerField(default=0)
+    geom = PolygonField()
+    
+    
