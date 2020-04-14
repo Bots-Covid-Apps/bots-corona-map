ea5bf40fcb7fa101b8f66fe3b5990c71fe6bd75d
Amey Deotale <22147603+GenericCoder19@users.noreply.github.com>
2020-04-14 12:16:14 -0400

    Added Models

--- covidmap/covidmap/settings.py
+++ covidmap/covidmap/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'jsonfield',
     'djgeojson',
     'leaflet',
+    'phonenumber_field',
     'gaborone'
 ]
 
--- covidmap/gaborone/models.py
+++ covidmap/gaborone/models.py
@@ -1,10 +1,35 @@
 from django.db import models
 from djgeojson.fields import PolygonField
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
 class MapRegion(models.Model):
     region_name = models.CharField(max_length=200)
     current_cases = models.IntegerField(default=0)
     geom = PolygonField()
+
+class Symptom(models.Model):
+    description = models.CharField(max_length=100)
+
+class Condition(models.Model):
+    description = models.CharField(max_length=100)
+
+class Support(models.Model):
+    description = models.CharField(max_length=100)
     
-    
+class Patient(models.Model):
+    GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other')
+    ]
+
+    phone_number = PhoneNumberField()
+    region = models.ForeignKey('MapRegion', on_delete=models.CASCADE)
+    age = models.IntegerField(default=0)
+    symptoms = models.ManyToManyField(Symptom)
+    condition = models.ManyToManyField(Condition)
+    has_travelled = models.BooleanField()
+    has_had_contact = models.BooleanField()
+    has_tested = models.BooleanField(null=True, blank=True)
+    gender = models.CharField(max_length=5, choices=GENDER_CHOICES)
